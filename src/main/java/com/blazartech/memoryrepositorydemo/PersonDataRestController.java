aefbbf7030732208192e7e5bd2b2181ecfcf7047
AARON <scottaaron@northwesternmutual.com>
2023-02-07 14:47:55 -0600

    added a POST request and made the filter call more realistic

--- src/main/java/com/blazartech/memoryrepositorydemo/PersonDataRestController.java
+++ src/main/java/com/blazartech/memoryrepositorydemo/PersonDataRestController.java
@@ -12,6 +12,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -34,9 +37,22 @@ public class PersonDataRestController {
         return l;
     }
     
-    @GetMapping("/person/{firstName}")
-    public List<PersonData> getPeopleByFirstName(@PathVariable String firstName) {
+    @GetMapping("/person/{id}")
+    public PersonData getPerson(@PathVariable Long id) {
+        log.info("getting person {}", id);
+        return repo.findById(id).get();
+    }
+    
+    @GetMapping(value = "/person", params = {"firstName"})
+    public List<PersonData> getPeopleByFirstName(@RequestParam String firstName) {
         log.info("getting people of first name {}", firstName);
         return repo.findByFirstName(firstName);
+    }
+    
+    @PostMapping("/person")
+    public PersonData addPerson(@RequestBody PersonData person) {
+        log.info("adding person {}", person);
+        repo.save(person);
+        return person;
     }
 }
